f502a1e0e53625b022596cfb78199bf118e65310
Denislav <evita10@abv.bg>
2021-02-22 05:36:47 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--Geting the frameworksCount from the computed, you can get a function the same way from the methods
+  Removing the :{{ index }} becouse we dont want to see the index part in the list  -->
+  <JsF />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import JsF from "./components/JsF";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { JsF },
+  computed: {
+    frameroksCount() {
+      return this.jsFrameworks.length;
+    },
+  },
+
+  methods: {
+    addFramework() {
+      //if statement prevanting from empty submiting inside the list
+      if (this.newFramework !== "") {
+        this.jsFrameworks.push({ name: this.newFramework });
+        this.newFramework = "";
+      }
+    },
+
+    remove(index) {
+      this.jsFrameworks = this.jsFrameworks.filter((hero, i) => i !== index);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/JsF.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="first">
+    <h1>Js Frameworks {{ frameroksCount }}</h1>
+    <ul>
+      <li v-for="(hero, index) in jsFrameworks" :key="index">
+        <div>
+          <p>
+            {{ hero.name }}
+            <button class="border rounded" @click="remove(index)">x</button>
+          </p>
+          <input />
+        </div>
+      </li>
+    </ul>
+    <form @submit.prevent="addFramework">
+      <input v-model="newFramework" placeholder="Type Framework Name" />
+      <button class="Submit" type="submit">Add Framework</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newFramework: "",
+      jsFrameworks: [
+        { name: "Vue" },
+        { name: "React" },
+        { name: "Angular" },
+        { name: "Node" },
+        { name: "Express" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
